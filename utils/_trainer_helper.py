ea489b97d5dc445705271de2151a1c2110ec1aff
francescocastelli <francesco8.castelli@mail.polimi.it>
2021-07-05 12:43:24 +0200

    fixed summary print

--- utils/_trainer_helper.py
+++ utils/_trainer_helper.py
@@ -18,18 +18,26 @@ def create_logs_dir(dir_path):
 
     return dir_path 
 
-def print_summary(model, device, args, eval_loop=False):
+def _print_model(model):
     width = os.get_terminal_size().columns
 
-    # model 
     out_m = '\n'
     out_m += '{:#^{}s}\n\n'.format('  Model: {}  '.format(model.name), width)
     print(out_m)
     print(model)
+
+def print_summary(model, device, args, multi_train, config_num, eval_loop=False):
+    width = os.get_terminal_size().columns
+
+    # model 
+    if not multi_train:
+        _print_model(model)
     
     # parameters 
+    conf = 'Configuration num: {}'.format(config_num)
+
     out = '\n\n'
-    out += '{:#^{}s}\n\n'.format('  Device: {}  '.format(device), width)
+    out += '{:#^{}s}\n\n'.format('  Device: {} - {} '.format(device, conf), width)
     for i, (key, value) in enumerate(vars(args).items()):
         out += '  {}={}  '.format(key, value) 
         if not (i+1) % 6:
@@ -38,6 +46,21 @@ def print_summary(model, device, args, eval_loop=False):
     print('\r{}\n'.format(out))
     if not eval_loop:
         print('{:#^{}s}{}'.format('  Training loop  ', width, '\n'*4))
+
+def print_overall_summary(model, device, train_config):
+    width = os.get_terminal_size().columns
+
+    # model 
+    _print_model(model)
+
+    # training configs
+    out = '\n\n'
+    out += '{:#^{}s}\n\n'.format('  Train configurations  ',  width)
+
+    for i, conf in enumerate(train_config):
+        out += ' Config {}: {}\n'.format(i, conf)
+
+    print('\r{}\n'.format(out))
 
 def print_epoch_stats(epoch, current_i, data_size, lr, end=False, **kwargs): 
     out = '  Epoch {}:\n'.format(epoch)
