c512a3e09e24678527da6f6211bd25e0c79a6242
francescocastelli <francesco8.castelli@mail.polimi.it>
2022-02-25 09:43:33 +0100

    fixed dataloader to accept valid sampler as none if train is set

--- torchtrainer/dataloader.py
+++ torchtrainer/dataloader.py
@@ -22,20 +22,33 @@ class TrainerLoader():
         self.g = torch.Generator()
         self.g.manual_seed(0)
 
-        if self.train_sampler is None and self.valid_sampler is not None:
-            raise ValueError("Valid sampler should be none since train sampler is None")
-
-        if self.train_sampler is not None and self.valid_sampler is None:
-            raise ValueError("Valid sampler should be none since train sampler is None")
-
-        self.default_sampler = self.train_sampler == self.valid_sampler == None
-
     def _worker_init_fn(self, w_id):
         worker_seed = torch.initial_seed() % 2**32
         random.seed(worker_seed)
         
         if self.worker_init is not None: 
             self.worker_init()
+
+    def _create_loader(self, dataset, sampler, device):
+        # if the device is a gpu we pin the memory on the dataloader for faster transfer of data
+        pin_mem = (device.type == 'cuda')
+        loader = torch.utils.data.DataLoader(dataset, batch_size=self.bs, 
+                                             sampler=sampler, 
+                                             num_workers=self.workers, 
+                                             collate_fn=self.collate_fn, 
+                                             pin_memory=pin_mem, 
+                                             worker_init_fn=self._worker_init_fn, 
+                                             generator=self.g)
+
+        return loader
+
+    def _get_default_sampler(self, dataset):
+        if self.shuffle:
+            return torch.utils.data.RandomSampler(dataset, 
+                                                  replacement=False, 
+                                                  generator=self.g)
+
+        return torch.utils.data.SequentialSampler(dataset)
 
     def _get_distributed_loader(self, dataset, world_size, rank, device):
         # set up distributed sampler
@@ -51,25 +64,12 @@ class TrainerLoader():
     def _get_loader(self, dataset, device, mode):
         assert mode == 'train' or mode == 'valid', "mode should either be train or valid"
 
-        # TODO: here we miss a default sampler if shuffle is false
-        if self.default_sampler and self.shuffle: 
-            sampler = torch.utils.data.RandomSampler(dataset, 
-                                                     replacement=False, 
-                                                     generator=self.g)
-        else: 
+        default = self.train_sampler == None if mode == 'train' else self.valid_sampler == None
+
+        if default:
+            sampler = self._get_default_sampler(dataset)
+        else:
             sampler = self.train_sampler if mode == 'train' else self.valid_sampler
 
         return self._create_loader(dataset, sampler, device)
 
-    def _create_loader(self, dataset, sampler, device):
-        # if the device is a gpu we pin the memory on the dataloader for faster transfer of data
-        pin_mem = (device.type == 'cuda')
-        loader = torch.utils.data.DataLoader(dataset, batch_size=self.bs, 
-                                             sampler=sampler, 
-                                             num_workers=self.workers, 
-                                             collate_fn=self.collate_fn, 
-                                             pin_memory=pin_mem, 
-                                             worker_init_fn=self._worker_init_fn, 
-                                             generator=self.g)
-
-        return loader
